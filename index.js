5889f6a2682f53d0ff0b43f179ee8652bd858f8d
unknown <manuel.elia.andres@gmail.com>
2023-02-05 21:04:52 +0100

    yes

--- index.js
+++ index.js
@@ -2,21 +2,41 @@ const fs = require("fs");
 const file = fs.readFileSync("dlList.txt", "utf-8");
 var exec = require("child_process").exec;
 
+//Additional settings
+let saveEmbededThumbnail = true;
+let useArchive = true;
+let additionalSettings = "";
+if (saveEmbededThumbnail) {
+  additionalSettings += "--embed-thumbnail";
+}
+if (useArchive) {
+  additionalSettings += "--download-archive FILE";
+}
+
+//Folder settings and download caller
 let dir = "none";
 file.split(/\r?\n/).forEach((line) => {
   if (!line.startsWith("http")) {
     dir = line;
   } else {
-    download(dir,line);
+    console.log(additionalSettings);
+    //download(dir, line);
   }
 });
 
-async function download(folder,link) {
+//Download function
+async function download(folder, link) {
   exec(
-    `youtube-dl -o "dl/${folder}/\%(title)s.%(ext)s" --extract-audio --audio-format mp3 ${link}`,
+    `youtube-dl -o "dl/${folder}/\%(title)s.%(ext)s"  --extract-audio --audio-format mp3 ${link}  --rm-cache-dir ${additionalSettings}`,
     function (error, stdout, stderr) {
       if (error !== null) {
-        console.log("exec error: " + link+" " +error);
+        console.log("exec error: " + link + " " + error);
+      }
+      if (stderr !== null) {
+        console.log("stderr: " + stderr);
+      }
+      if (stdout !== null) {
+        console.log("stdout: " + stdout);
       }
     }
   );
